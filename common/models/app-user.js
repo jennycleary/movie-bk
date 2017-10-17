fb147a852b9923fa3ca8cab2e04dd5f8de4a9aa0
Peter Horton <phorton@softstackfactory.org>
2017-10-17 21:41:30 +0000

    added moviesdb service

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Appuser) {
-
+    Appuser.afterRemote('login', async (ctx, user, next) => {
+        if(user){
+            console.log("hit")
+            user.token = user.id;
+        
+            let userData = await Appuser.find({
+                    fields:{password: false, username: false, realm: false, '_id': 0},
+                    include:{
+                        relation: 'movies',
+                        scope: {
+                            fields: ['title', 'score']
+                        },
+                    },
+                    where: {
+                    id: user.userId
+                }
+            })
+            console.log()
+            user.userData = userData[0]
+        }
+    })
 };
